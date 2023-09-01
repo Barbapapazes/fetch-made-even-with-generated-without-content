8ad6202eb8666f3dafdfb6673b8e956c7ed16886
barbapapazes <e.soubiran25@gmail.com>
2023-09-01 16:01:45 +0200

    chore: initial commit

---
+++ pages/about.vue
@@ -0,0 +1,11 @@
+<script lang="ts" setup>
+const { data } = await useFetch('https://ungh.cc/repos/unjs/nitro')
+</script>
+
+<template>
+  <div>
+    {{ data }}
+    <h1> About Page</h1>
+    <NuxtLink to="/">Home</NuxtLink>
+  </div>
+</template>
