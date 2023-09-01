482ba76e3528857ec36955b31cf235ecb8396e45
barbapapazes <e.soubiran25@gmail.com>
2023-09-01 16:07:56 +0200

    feat: use layout

---
+++ layouts/custom.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+const { data } = useFetch('https://ungh.cc/repos/unjs/nitro')
+
+const { data: data2 } = useAsyncData('data', () => $fetch('https://ungh.cc/repos/unjs/nitro'))
+</script>
+
+<template>
+  <div>
+    {{ data }}
+    {{ data2 }}
+    <p>Custom Layout</p>
+    <hr/>
+    <slot />
+  </div>
+</template>
--- pages/about.vue
+++ pages/about.vue
@@ -1,10 +1,11 @@
 <script lang="ts" setup>
-const { data } = await useFetch('https://ungh.cc/repos/unjs/nitro')
+definePageMeta({
+  layout: 'custom'
+})
 </script>
 
 <template>
   <div>
-    {{ data }}
     <h1> About Page</h1>
     <NuxtLink to="/">Home</NuxtLink>
   </div>
